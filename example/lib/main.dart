16172758d5d09867f45c75c798ec0fb98f3c59c8
Harsh Bhikadia <harsh.bhikadiya@gmail.com>
2018-07-21 23:25:32 +0530

    example: minor fix

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -49,7 +49,7 @@ class _MyHomePageState extends State<MyHomePage>
         child: Text("Backpanel"),
       ),
       body: Center(
-        child: Text("Backpanel"),
+        child: Text("Body"),
       ),
     );
   }
