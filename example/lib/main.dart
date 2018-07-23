3f368f4195d6a0fb630725da15796f51a7ae41b5
Harsh Bhikadia <harsh.bhikadiya@gmail.com>
2018-07-24 00:55:41 +0530

    shorter example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -8,31 +8,14 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return new MaterialApp(
       title: 'Backdrop Demo',
-      theme: new ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: new MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key}) : super(key: key);
-
-  @override
-  _MyHomePageState createState() => new _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return BackdropScaffold(
-      title: Text("Backdrop Example"),
-      backpanel: Center(
-        child: Text("Backpanel"),
-      ),
-      body: Center(
-        child: Text("Body"),
+      home: BackdropScaffold(
+        title: Text("Backdrop Example"),
+        backpanel: Center(
+          child: Text("Backpanel"),
+        ),
+        body: Center(
+          child: Text("Body"),
+        ),
       ),
     );
   }
