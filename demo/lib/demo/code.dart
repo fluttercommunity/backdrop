384a3ccfc6502fcc09a96f43804d86880f17746f
Felix Wielander <felix.wielander@gmail.com>
2020-06-27 12:16:53 +0200

    Added syntax highlighting for code using flutter_highlight

--- demo/lib/demo/code.dart
+++ demo/lib/demo/code.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_highlight/flutter_highlight.dart';
+import 'package:flutter_highlight/themes/dracula.dart';
 
 class Code extends StatefulWidget {
   String codeFile;
@@ -20,7 +22,17 @@ class _CodeState extends State<Code> {
         future: DefaultAssetBundle.of(context).loadString(widget.codeFile),
         builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
           if (snapshot.hasData) {
-            return SingleChildScrollView(child: Text(snapshot.data));
+            return SingleChildScrollView(
+              child: HighlightView(
+                snapshot.data,
+                language: "dart",
+                theme: draculaTheme,
+                padding: EdgeInsets.all(8.0),
+                textStyle: TextStyle(
+                  fontSize: 11,
+                ),
+              ),
+            );
           } else if (snapshot.hasError) {
             return Text(snapshot.error.toString());
           } else {
