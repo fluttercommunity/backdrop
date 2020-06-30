a07e41a91e9735ed3618998ca8fbb984fad1abee
Felix Wielander <felix.wielander@gmail.com>
2020-06-30 17:28:42 +0200

    Finished contextual info use case

--- demo/lib/data/use_cases/contextual_info/contextual_info.dart
+++ demo/lib/data/use_cases/contextual_info/contextual_info.dart
@@ -1,8 +1,23 @@
 import 'package:backdrop/backdrop.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
+import 'package:flutter/rendering.dart';
 
 /// Contextual info preview app.
-class ContextualInfo extends StatelessWidget {
+class ContextualInfo extends StatefulWidget {
+  @override
+  _ContextualInfoState createState() => _ContextualInfoState();
+}
+
+class _ContextualInfoState extends State<ContextualInfo> {
+  Widget _selectedProduct;
+
+  @override
+  void initState() {
+    _selectedProduct = null;
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -11,17 +26,166 @@ class ContextualInfo extends StatelessWidget {
       home: BackdropScaffold(
         appBar: BackdropAppBar(
           title: Text("Contextual Info Example"),
+          automaticallyImplyLeading: false,
         ),
-        backLayer: Center(
-          child: Text("Back Layer"),
-        ),
-        subHeader: BackdropSubHeader(
-          title: Text("Sub Header"),
-        ),
-        frontLayer: Center(
-          child: Text("Contextual Info"),
-        ),
+        backLayer: _createBackLayer(),
+        frontLayer: _createFrontLayer(),
+        stickyFrontLayer: true,
       ),
     );
   }
+
+  Widget _createBackLayer() => ListView(
+        children: [
+          ListTile(
+            leading: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "Name",
+                ),
+              ],
+            ),
+            title: Text(
+              "Laptop Model X",
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+          ),
+          ListTile(
+            leading: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text("Year of production"),
+              ],
+            ),
+            title: Text(
+              "2019",
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+          ),
+          ListTile(
+            leading: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text("Place of Manufacture"),
+              ],
+            ),
+            title: Text(
+              "USA",
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+          ),
+          ListTile(
+            leading: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text("Price"),
+              ],
+            ),
+            title: Text(
+              "\$999",
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+          ),
+        ],
+        shrinkWrap: true,
+      );
+
+  Widget _createFrontLayer() => Container(
+        margin: EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            Row(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Icon(
+                  Icons.computer,
+                  size: 64.0,
+                ),
+                Text(
+                  "Laptop",
+                  style: Theme.of(context)
+                      .textTheme
+                      .headline3
+                      .apply(color: Colors.black),
+                ),
+              ],
+            ),
+            Row(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "Price",
+                  style: TextStyle(color: Colors.grey),
+                ),
+                Container(
+                  margin: EdgeInsets.all(8.0),
+                  child: Text(
+                    "\$999",
+                    style: TextStyle(fontSize: 18),
+                  ),
+                ),
+              ],
+            ),
+            Container(
+              margin: EdgeInsets.all(8.0),
+              child: Row(
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Row(
+                    children: [
+                      Icon(Icons.star, color: Colors.grey),
+                      Icon(Icons.star, color: Colors.grey),
+                      Icon(Icons.star, color: Colors.grey),
+                      Icon(Icons.star, color: Colors.grey),
+                      Icon(Icons.star_half, color: Colors.grey),
+                    ],
+                  ),
+                  Text(
+                    "73 Reviews",
+                    style: TextStyle(color: Colors.grey),
+                  )
+                ],
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.all(16.0),
+              child: Row(
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Builder(
+                    builder: (context) => RaisedButton(
+                      child: Text("More about this product"),
+                      onPressed: () => Backdrop.of(context).fling(),
+                    ),
+                  )
+                ],
+              ),
+            ),
+            ListTile(
+              title: Text("Reviews",
+                  style: Theme.of(context)
+                      .textTheme
+                      .headline6
+                      .apply(color: Colors.black)),
+            ),
+            ListTile(
+              leading: Icon(Icons.account_box),
+              title: Text("Really satisfied!"),
+              subtitle: Text("John Doe"),
+              trailing: Text("5/5"),
+            ),
+            ListTile(
+              leading: Icon(Icons.account_box),
+              title: Text("Good price!"),
+              subtitle: Text("Jane Doe"),
+              trailing: Text("4.5/5"),
+            )
+          ],
+        ),
+      );
 }
