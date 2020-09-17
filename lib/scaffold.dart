173008db24f7e277b701679c768bab2aade68cf8
kannel-outis <emirbadoo@gmail.com>
2020-09-17 16:07:41 -0700

    ......

--- lib/scaffold.dart
+++ lib/scaffold.dart
@@ -237,6 +237,12 @@ class BackdropScaffold extends StatefulWidget {
   /// Defaults to `true`.
   final bool endDrawerEnableOpenDragGesture;
 
+  ///
+  ///
+  ///
+  ///it uses the default GlobalKey if null
+  final Key backDropScaffoldKey;
+
   /// Creates a backdrop scaffold to be used as a material widget.
   BackdropScaffold({
     Key key,
@@ -286,6 +292,7 @@ class BackdropScaffold extends StatefulWidget {
     this.drawerEdgeDragWidth,
     this.drawerEnableOpenDragGesture = true,
     this.endDrawerEnableOpenDragGesture = true,
+    this.backDropScaffoldKey,
   })  : assert(inactiveOverlayOpacity >= 0.0 && inactiveOverlayOpacity <= 1.0),
         super(key: key);
 
@@ -539,7 +546,7 @@ class BackdropScaffoldState extends State<BackdropScaffold>
     return WillPopScope(
       onWillPop: () => _willPopCallback(context),
       child: Scaffold(
-        key: scaffoldKey,
+        key: widget.backDropScaffoldKey ?? scaffoldKey,
         appBar: widget.appBar ??
             AppBar(
               title: widget.title,
