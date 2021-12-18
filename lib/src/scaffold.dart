22abacac185c38467533a6f6a8053008a99946bd
Harsh Bhikadia <harsh@bhikadia.com>
2021-12-18 16:31:29 +0530

    minor

--- lib/src/scaffold.dart
+++ lib/src/scaffold.dart
@@ -1,4 +1,4 @@
-import 'package:backdrop/app_bar.dart';
+import 'package:backdrop/backdrop.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
