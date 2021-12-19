59eb3ca231cc278e4195440baa454f8aeb304167
Harsh Bhikadia <harsh.bhikadiya@gmail.com>
2021-12-19 15:35:16 +0530

    - ci: fix - install flutter at $HOME - instead of inside project

--- .github/workflows/scripts/install-flutter.sh
+++ .github/workflows/scripts/install-flutter.sh
@@ -2,5 +2,5 @@
 
 BRANCH=$1
 
-git clone https://github.com/flutter/flutter.git --depth 1 -b $BRANCH _flutter
-echo "$GITHUB_WORKSPACE/_flutter/bin" >> $GITHUB_PATH
+git clone https://github.com/flutter/flutter.git --depth 1 -b $BRANCH $HOME/_flutter
+echo "$HOME/_flutter/bin" >> $GITHUB_PATH
