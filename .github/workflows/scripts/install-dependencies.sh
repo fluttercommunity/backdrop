ed5d462eb5b2d6228596a309ba8196caad012b18
Harsh Bhikadia <harsh.bhikadiya@gmail.com>
2021-12-19 16:15:01 +0530

    - using `subosito/flutter-action` to install flutter - removed `install-flutter.sh` and `install-dependencies.sh`

--- .github/workflows/scripts/install-dependencies.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-flutter config --no-analytics
-flutter pub get
-(cd demo && flutter pub get)
-echo "$HOME/.pub-cache/bin" >> $GITHUB_PATH
-echo "$HOME/_flutter/.pub-cache/bin" >> $GITHUB_PATH
-echo "$HOME/_flutter/bin/cache/dart-sdk/bin" >> $GITHUB_PATH
--- .github/workflows/scripts/install-flutter.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-BRANCH=$1
-
-git clone https://github.com/flutter/flutter.git --depth 1 -b $BRANCH $HOME/_flutter
-echo "$HOME/_flutter/bin" >> $GITHUB_PATH
